a9f1516cd5826b9fafc201918a90c0395648ced9
이아름 <areum6321@gmail.com>
2024-07-01 17:05:29 +0900

    Servlet - Servlet 등록하기

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); //단순문자 - 헤더 정보에 삽입
+        response.setCharacterEncoding("utf-8"); //인코딩정보 - 헤더 정보에 삽입
+        response.getWriter().write("hello " + username); //http body에 삽입
+    }
+}
